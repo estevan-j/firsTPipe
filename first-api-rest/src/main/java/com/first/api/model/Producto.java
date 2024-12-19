6da7e122a0c7c6ece02d7e697aeaed56d716d5ac
esteb <esteban.bajana@epn.edu.ec>
2024-12-19 14:23:26 -0500

    fix setter

--- first-api-rest/src/main/java/com/first/api/model/Producto.java
+++ first-api-rest/src/main/java/com/first/api/model/Producto.java
@@ -27,4 +27,20 @@ public class Producto {
     public double getPrecio() {
         return precio;
     }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 }
